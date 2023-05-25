5f04d73882bc85f3ce7fb3b3281277f04d1ed5b9
deepikakgowda <deepikakgowda37@gmail.com>
2023-05-25 13:36:07 +0530

    Add project files.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Message from './component/Message'
+
+function App() {
+  
+
+  return <Message/>
+    
+}
+
+export default App;
